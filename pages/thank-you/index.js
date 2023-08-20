20f96eeb030e8b6390076dc07e82ff21e10a2621
Lakshya Swarup <129587128+Aim-Swarup@users.noreply.github.com>
2023-08-21 02:50:17 +0530

    Update index.js

--- pages/thank-you/index.js
+++ pages/thank-you/index.js
@@ -18,7 +18,7 @@ const Contact = () => {
     <div className='h-full bg-primary/30'>
       <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center xl:justify-end h-full gap-x-6'>
         {/* text & form */}
-        <div className='flex flex-col w-full xl:w-full max-w-[700px] z-30'>
+        <div className='flex flex-col w-full xl:w-full max-w-[1000px] z-30'>
           {/* text */}
           <motion.h2 
             variants={fadeIn('up', 0.2)}
@@ -27,7 +27,7 @@ const Contact = () => {
             exit='hidden'
             className='h2 text-center mb-12'
           >
-            Thank <span className='text-accent'>you,</span> form submitted sucessfully!
+            Thank <span className='text-accent'>you,</span> message sent sucessfully!
           </motion.h2>
         </div>
       </div>
