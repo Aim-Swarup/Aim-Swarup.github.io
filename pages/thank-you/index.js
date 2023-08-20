bca48e41cf02facd76ae9459d96a6a48e8c1331f
Lakshya Swarup <129587128+Aim-Swarup@users.noreply.github.com>
2023-08-21 02:47:31 +0530

    Update index.js

--- pages/thank-you/index.js
+++ pages/thank-you/index.js
@@ -18,7 +18,7 @@ const Contact = () => {
     <div className='h-full bg-primary/30'>
       <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center xl:justify-end h-full gap-x-6'>
         {/* text & form */}
-        <div className='flex flex-col w-full xl:w-[65%] max-w-[700px] z-30'>
+        <div className='flex flex-col w-full xl:w-full max-w-[700px] z-30'>
           {/* text */}
           <motion.h2 
             variants={fadeIn('up', 0.2)}
