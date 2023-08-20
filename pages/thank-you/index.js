45551b16a9b7a7d4d50158b3c3c4ecd46d77df94
Lakshya Swarup <129587128+Aim-Swarup@users.noreply.github.com>
2023-08-21 02:44:38 +0530

    Update index.js

--- pages/thank-you/index.js
+++ pages/thank-you/index.js
@@ -1,1 +1,38 @@
+// icons
+import { BsArrowRight } from 'react-icons/bs'
 
+// framer
+import { motion } from 'framer-motion'
+
+// next image
+import Image from 'next/image';
+
+// react
+import React, { useState } from 'react';
+
+// variants
+import {fadeIn} from '../../variants'
+
+const Contact = () => {
+  return (
+    <div className='h-full bg-primary/30'>
+      <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center xl:justify-end h-full gap-x-6'>
+        {/* text & form */}
+        <div className='flex flex-col w-full xl:w-[65%] max-w-[700px] z-30'>
+          {/* text */}
+          <motion.h2 
+            variants={fadeIn('up', 0.2)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='h2 text-center mb-12'
+          >
+            Thank <span className='text-accent'>you,</span> form submitted sucessfully!
+          </motion.h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
