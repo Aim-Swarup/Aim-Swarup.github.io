31868a8a8a8c7424bef3773ee376e3ee5e76fd34
Lakshya Swarup <129587128+Aim-Swarup@users.noreply.github.com>
2023-08-21 02:57:12 +0530

    Update index.js

--- pages/thank-you/index.js
+++ pages/thank-you/index.js
@@ -16,7 +16,7 @@ import {fadeIn} from '../../variants'
 const Contact = () => {
   return (
     <div className='h-full bg-primary/30'>
-      <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center xl:justify-end h-full gap-x-6'>
+      <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center xl:justify-center h-full gap-x-6'>
         {/* text & form */}
         <div className='flex flex-col w-full xl:w-full max-w-[1000px] z-30'>
           {/* text */}
